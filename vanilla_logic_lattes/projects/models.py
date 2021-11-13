f6729575e21cc977359d86250c946b0355d87830
Georgie Dignan <georgiedignan@gmail.com>
2021-11-13 11:23:23 +1000

    added models for canvas and sticky note

--- vanilla_logic_lattes/projects/models.py
+++ vanilla_logic_lattes/projects/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+class Canvas(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.URLField()
+    is_open = models.BooleanField()
+    date_created = models.DateField(default=date.today)
+    team_name = models.CharField(max_length=200)
+
+
+class StickyNote(models.Model):
+    who = models.CharField(max_length=200)
+    what = models.TextField()
+    why = models.TextField()
+    anonymous = models.BooleanField()
+    date_created = models.DateField(default=date.today)
+
