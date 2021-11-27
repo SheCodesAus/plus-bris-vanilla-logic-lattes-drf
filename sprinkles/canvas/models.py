aa87dd8283b09e7432c7489f1dc5cbb5fb31a4f5
AzureAD\BrunaSergio <bruna.sergio@mslsolutions.com>
2021-11-27 11:55:37 +1000

    add default image

--- sprinkles/canvas/models.py
+++ sprinkles/canvas/models.py
@@ -9,7 +9,7 @@ class Canvas(models.Model):
     is_public = models.BooleanField(null=True)
     date_created = models.DateField(default=date.today)
     owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, default=1)
-    image = models.URLField()
+    image = models.URLField(default="https://images.unsplash.com/photo-1626094309830-abbb0c99da4a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80")
 
 class StickyNote(models.Model):
     title = models.CharField(max_length=200, null=True)
